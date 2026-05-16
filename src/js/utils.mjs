55ae459d59d00844931d52e756a23db13e568fc9
wilfredo-marapacuto <wmarapacutoverastegu@byupathway.edu>
2026-05-15 18:47:05 -0600

    Dynamic product list

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -2,17 +2,17 @@
 export function qs(selector, parent = document) {
   return parent.querySelector(selector);
 }
-// or a more concise version if you are into that sort of thing:
-// export const qs = (selector, parent = document) => parent.querySelector(selector);
 
-// retrieve data from localstorage
+// retrieve data from localStorage
 export function getLocalStorage(key) {
   return JSON.parse(localStorage.getItem(key));
 }
-// save data to local storage
+
+// save data to localStorage
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener('touchend', (event) => {
@@ -22,12 +22,27 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener('click', callback);
 }
 
-//Function to get param for product
-
+// function to get param from URL
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product');
+  const value = urlParams.get(param);
 
-  return product;
+  return value;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = 'afterbegin',
+  clear = false,
+) {
+  if (clear) {
+    parentElement.innerHTML = '';
+  }
+
+  const htmlStrings = list.map(templateFn);
+
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(''));
 }
